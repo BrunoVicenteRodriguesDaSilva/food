c80eb3a8a944cfc19045ffffb1adaeedb7c522cc
Bruno Vicente <brunovicente.dev@gmail.com>
2023-01-19 13:53:21 -0300

    Added Image and Infos

---
+++ src/Components/ResultsDetail.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { View, Image, Text, StyleSheet } from "react-native";
+
+const ResultsDetail = ({ result }) => {
+  return (
+    <View style={styles.Container}>
+      <Image source={{ uri: result.image_url }} style={styles.Image} />
+      <Text style={styles.Name}>{result.name}</Text>
+      <Text style={styles.Rating}>
+        {result.rating} Stars, {result.review_count} Reviews
+      </Text>
+      <Text style={styles.Address}>{result.location.address1} </Text>
+      {result.location.address2 ? (
+        <Text style={styles.Address}>{result.location.address2} </Text>
+      ) : null}
+      {result.location.address3 ? (
+        <Text style={styles.Address}>{result.location.address3} </Text>
+      ) : null}
+      <Text style={styles.City}>{result.location.city} </Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  Container: {
+    marginLeft: 10,
+    width: 250,
+  },
+  Image: {
+    width: 250,
+    height: 150,
+    borderRadius: 4,
+  },
+  Name: {
+    fontWeight: "bold",
+  },
+  Rating: {
+    fontWeight: "bold",
+    fontSize: 12,
+  },
+  Address: {
+    fontSize: 12,
+  },
+  City: {
+    color: "#adadad",
+    fontSize: 12,
+  },
+});
+
+export default ResultsDetail;
