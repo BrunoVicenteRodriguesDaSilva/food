6c621c51bbe622b560c465d66a3960a557949b25
Bruno Vicente <brunovicente.dev@gmail.com>
2023-01-19 10:10:00 -0300

    Added API request to SearchScreen

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,20 +1,34 @@
 import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import SearchBar from "../Components/SearchBar";
+import yelp from "../API/yelp";
 
 const SearchScreen = () => {
   const [term, setTerm] = useState("");
+  const [results, setResults] = useState([]);
+
+  const searchAPI = async () => {
+    const response = await yelp.get("/search", {
+      params: {
+        limit: 50,
+        term: term,
+        location: "sao paulo",
+      },
+    });
+    setResults(response.data.businesses);
+  };
 
   return (
     <View style={styles.DIV}>
       <SearchBar
-        onTermSubmit={() => console.log("done")}
+        onTermSubmit={searchAPI}
         term={term}
         onTermChange={(newTerm) => {
           setTerm(newTerm);
         }}
       />
       <Text>Search Screen</Text>
+      <Text>We have found {results.length} results</Text>
     </View>
   );
 };
