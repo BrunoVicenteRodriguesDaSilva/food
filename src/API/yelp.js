4933144be13b1a3084a44cef5bb490687b8760f3
Bruno Vicente <brunovicente.dev@gmail.com>
2023-01-31 13:42:54 -0300

    Deleted comment

--- src/API/yelp.js
+++ src/API/yelp.js
@@ -7,9 +7,3 @@ export default axios.create({
     Authorization: `Bearer ${AUTHORIZATION}`,
   },
 });
-
-// baseURL: ,
-//   headers: {
-//     Authorization:
-//       "Bearer wREFf92OiQRRKUDUZlK2qZJZuFtmLWGXaYjkentJMjS3r9Dws8q_Ye9y0b0D3-dsBapafX13c6SZTsnwjuUYw9NyHFYFnJxTU-bh31WbrhocUgtcnI2_Zts4ufTHY3Yx",
-//   },
